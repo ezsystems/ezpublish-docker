e8e2377401b5978953bda6f82e312cc2b29074dc
kibao <kibao.pl@gmail.com>
2013-12-23 23:05:04 +0100

    Sample build.sh

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker build -rm -t sample/apache dockerfiles/apache
+
+docker build -rm -t sample/apache-php:prod dockerfiles/apache-php/prod
+docker build -rm -t sample/apache-php:dev  dockerfiles/apache-php/dev
+
+docker build -rm -t sample/application:dev  dockerfiles/application/dev
+docker build -rm -t sample/application:prod application
+
+
+
