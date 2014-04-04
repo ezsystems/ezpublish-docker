0343831f905dc099081e06ddaaf07c9288cb2c4a
André R <ar@ez.no>
2014-04-04 14:56:51 +0200

    Move application:prod to dockerfiles

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ docker build -rm -t sample/apache-php:prod dockerfiles/apache-php/prod
 docker build -rm -t sample/apache-php:dev  dockerfiles/apache-php/dev
 
 docker build -rm -t sample/application:dev  dockerfiles/application/dev
-docker build -rm -t sample/application:prod application
+docker build -rm -t sample/application:prod dockerfiles/application/prod
 
 
 
