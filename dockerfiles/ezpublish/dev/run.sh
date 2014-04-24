cf8da4bb1c3511552a4dde701b7d7dc0271939e7
André R <ar@ez.no>
2014-04-24 19:47:02 +0200

    Add permision for boot up to make sure ezp has right file permissioms

--- dockerfiles/ezpublish/dev/run.sh
+++ dockerfiles/ezpublish/dev/run.sh
@@ -4,4 +4,11 @@ if [ "$EZ_KICKSTART" = "true" ]; then
 	/generate_kickstart_file.sh
 fi
 
+# Fix permissions as they are wrong in case rsync is used
+cd /var/www/
+sudo chown -R "${APACHE_RUN_USER}":"${APACHE_RUN_GROUP}" ezpublish/{cache,logs,config} ezpublish_legacy/{design,extension,settings,var} web
+sudo find {ezpublish/{cache,logs,config},ezpublish_legacy/{design,extension,settings,var},web} -type d | sudo xargs chmod -R 775
+sudo find {ezpublish/{cache,logs,config},ezpublish_legacy/{design,extension,settings,var},web} -type f | sudo xargs chmod -R 664
+
+# start services
 exec supervisord -n
