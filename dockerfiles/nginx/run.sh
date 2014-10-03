c76860536c5b1166a43ad515f4e0f99704f6fb51
Vidar Langseid <vl@ez.no>
2014-10-03 15:38:08 +0200

    Setting nginx setting FASTCGI_READ_TIMEOUT according to php's max_execution_time

--- dockerfiles/nginx/run.sh
+++ dockerfiles/nginx/run.sh
@@ -9,12 +9,13 @@ ln -s /etc/nginx/sites-available/ezpublish /etc/nginx/sites-enabled/ezpublish
 
 # Make sure nginx forwards to php5-fpm on tcp port, not unix socket
 sed -i "s@  fastcgi_pass unix:/var/run/php5-fpm.sock;@  # fastcgi_pass unix:/var/run/php5-fpm.sock;@" /etc/nginx/sites-available/ezpublish
-sed -i "s@  #fastcgi_pass 127.0.0.1:9000;@  fastcgi_pass ${PHP_FPM_PORT_9000_TCP_ADDR}:${PHP_FPM_PORT_9000_TCP_PORT};@" /etc/nginx/sites-available/ezpublish
+sed -i "s@  #fastcgi_pass 127.0.0.1:9000;@  fastcgi_pass php_fpm:${PHP_FPM_PORT_9000_TCP_PORT};@" /etc/nginx/sites-available/ezpublish
 
 # Update port number and basedir in site-available/ezpublish
 sed -i "s@%PORT%@${PORT}@" /etc/nginx/sites-available/ezpublish
 sed -i "s@%BASEDIR%@${BASEDIR}@" /etc/nginx/sites-available/ezpublish
 
+echo "fastcgi_read_timeout $FASTCGI_READ_TIMEOUT;" > /etc/nginx/conf.d/fastcgi_read_timeout.conf
 
 #echo "while true; do echo hello world; sleep 1; done" > /a.sh; chmod a+x /a.sh
 #/a.sh
