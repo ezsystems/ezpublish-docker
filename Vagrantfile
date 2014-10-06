ezsystems/ezpublish-docker

commit fdf43d70a94156277117bec4c21fff7ad4235ca2

Removed setting kickstart and package url environement variables for php-fpm and nginx container. These env variables are only used in prepare container

Vagrantfile
@@ -47,10 +47,10 @@ Vagrant.configure("2") do |config|
       daemonize: false
     d.run "php-fpm",
       image: "ezsystems/php-fpm",
-      args: "--link db-1:db --dns 8.8.8.8 --dns 8.8.4.4 --volumes-from ezpublish-vol -e EZ_KICKSTART=\""+ vagrantConfig['ezpublish']['kickstart'] +"\" -e EZ_PACKAGEURL=\""+ vagrantConfig['ezpublish']['packageurl'] +"\""
+      args: "--link db-1:db --dns 8.8.8.8 --dns 8.8.4.4 --volumes-from ezpublish-vol
     d.run "web-nginx",
       image: "ezsystems/nginx",
-      args: "--link php-fpm:php_fpm --dns 8.8.8.8 --dns 8.8.4.4 -p 80:80 --volumes-from ezpublish-vol -e EZ_KICKSTART=\""+ vagrantConfig['ezpublish']['kickstart'] +"\" -e EZ_PACKAGEURL=\""+ vagrantConfig['ezpublish']['packageurl'] +"\""
+      args: "--link php-fpm:php_fpm --dns 8.8.8.8 --dns 8.8.4.4 -p 80:80 --volumes-from ezpublish-vol
   end
 
   ssh_authorized_keys_file = File.read( "files/authorized_keys2" )

commit c47cd94fd10ce814f4447555561bcf00c99c6e23

Added missing quotes in 'args:' line in Vagrant file ( bug introduced in fdf43d70a94156277117bec4c21fff7ad4235ca2 )

Vagrantfile
@@ -47,10 +47,10 @@ Vagrant.configure("2") do |config|
       daemonize: false
     d.run "php-fpm",
       image: "ezsystems/php-fpm",
-      args: "--link db-1:db --dns 8.8.8.8 --dns 8.8.4.4 --volumes-from ezpublish-vol
+      args: "--link db-1:db --dns 8.8.8.8 --dns 8.8.4.4 --volumes-from ezpublish-vol"
     d.run "web-nginx",
       image: "ezsystems/nginx",
-      args: "--link php-fpm:php_fpm --dns 8.8.8.8 --dns 8.8.4.4 -p 80:80 --volumes-from ezpublish-vol
+      args: "--link php-fpm:php_fpm --dns 8.8.8.8 --dns 8.8.4.4 -p 80:80 --volumes-from ezpublish-vol"
   end
 
   if vagrantConfig['debug']['copy_authorized_keys2'] == false