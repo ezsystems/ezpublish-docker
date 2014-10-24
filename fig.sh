546bc48455c16e36f9f759eccee924da6d8848c3
Vidar Langseid <vl@ez.no>
2014-10-24 15:01:49 +0200

    Fixed fig.sh. Parameters used when running fig_initial.yml was wrong

--- fig.sh
+++ fig.sh
@@ -4,6 +4,6 @@ cp files/auth.json dockerfiles/ezpublish/prepare
 cp files/etcd_functions dockerfiles/mysql
 
 # This is a workaround for https://github.com/docker/fig/issues/540
-fig -d -f fig_initial.yml up
+fig -f fig_initial.yml "$@"
 
 fig "$@"
