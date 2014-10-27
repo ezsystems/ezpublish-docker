d8912b32eb78bfdb5f2683e1f7c4463a925fc31b
Vidar Langseid <vl@ez.no>
2014-10-27 16:39:21 +0100

    Made sure etcd image is build before other images and that the .deb file is copied to other images that need it

--- fig.sh
+++ fig.sh
@@ -6,4 +6,14 @@ cp files/etcd_functions dockerfiles/mysql
 # This is a workaround for https://github.com/docker/fig/issues/540
 fig -f fig_initial.yml "$@"
 
+# We need to build etcd next so that the .deb package can be placed inside other images
+if [ ! -f volumes/etcd/etcd_0.4.6_amd64.deb ]; then
+    fig -f fig_etcd.yml "$@"
+fi
+
+# Copy the etcd .deb to the dockerfile directory for images that need it
+if [ ! -f dockerfiles/mysql/etcd_0.4.6_amd64.deb ]; then
+    cp volumes/etcd/etcd_0.4.6_amd64.deb dockerfiles/mysql
+fi
+
 fig "$@"
