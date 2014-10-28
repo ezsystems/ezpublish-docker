c15da51ae88ff6de16e975f62f0c0953038868aa
Vidar Langseid <vl@ez.no>
2014-10-28 10:00:37 +0100

    Added etcd_functions to dockerfiles/etcd

--- fig.sh
+++ fig.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cp files/auth.json dockerfiles/ezpublish/prepare
+cp files/etcd_functions dockerfiles/etcd
 cp files/etcd_functions dockerfiles/mysql
 
 # This is a workaround for https://github.com/docker/fig/issues/540
