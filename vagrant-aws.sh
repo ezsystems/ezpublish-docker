367ad5a3283f903bf7c1bc5135d85d2615533dce
Vidar Langseid <vl@ez.no>
2014-12-03 10:32:14 +0100

    Removed unwanted 'ls -all' in vagrant wrapper

--- vagrant-aws.sh
+++ vagrant-aws.sh
@@ -7,7 +7,6 @@ cat <<EOT > $tmpscript
 #!/bin/bash
 
 cd /vagrant
-ls -all
 EOT
 echo vagrant "$@" >> $tmpscript
 chmod a+x $tmpscript
@@ -20,4 +19,4 @@ gname=`id -g --name`
 #docker run -i -t --rm --volume $SCRIPTDIR:/vagrant --volume $tmpscript:/tmp/vagrant_execute.sh --volume $HOME/.ssh:/home/$username/.ssh ezpublishdocker_vagrantaws:latest bash -c "groupadd $gname --gid $gid; useradd $username --uid $uid --gid $gid; chown $username:$gname /home/$username;  mv /root/.vagrant.d /home/$username/; chown -R $username:$gname /home/$username/.vagrant.d; cd /vagrant; sudo -i -u $username bash"
 docker run -i -t --rm --volume $SCRIPTDIR:/vagrant --volume $tmpscript:/tmp/vagrant_execute.sh --volume $HOME/.ssh:/home/$username/.ssh ezpublishdocker_vagrantaws:latest bash -c "groupadd $gname --gid $gid; useradd $username --uid $uid --gid $gid; chown $username:$gname /home/$username; mv /root/.vagrant.d /home/$username/; chown -R $username:$gname /home/$username/.vagrant.d; cd /vagrant; sudo -i -u $username /tmp/vagrant_execute.sh"
 
-m $tmpscript
+rm $tmpscript
