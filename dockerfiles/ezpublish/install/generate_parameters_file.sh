0fc6187dc72959bc86b54f1fcc929fe7c78e6a7b
Vidar Langseid <vl@ez.no>
2015-02-18 10:30:24 +0100

    Removed parameters.yml.template and instead patching the parameters.yml which is provided in dist

--- dockerfiles/ezpublish/install/generate_parameters_file.sh
+++ dockerfiles/ezpublish/install/generate_parameters_file.sh
@@ -15,9 +15,12 @@ SECRET=`generate_secret`
 echo secret : $SECRET
 echo dbpasswd : ${MYSQL_PASS}
 
-cp /parameters.yml.template ezpublish/config/parameters.yml
-sed -i "s@%secret%@$SECRET@" ezpublish/config/parameters.yml
-sed -i "s@%dbpassword%@${MYSQL_PASS}@" ezpublish/config/parameters.yml
+sed -i "s@secret:.*@secret: $SECRET@" ezpublish/config/parameters.yml
+sed -i "s@database_host:.*@database_host: db@" ezpublish/config/parameters.yml
+sed -i "s@database_name:.*@database_name: ezp@" ezpublish/config/parameters.yml
+sed -i "s@database_user:.*@database_user: admin@" ezpublish/config/parameters.yml
+sed -i "s@database_password:.*@database_password: $MYSQL_PASS@" ezpublish/config/parameters.yml
+
 
 
 cat ezpublish/config/parameters.yml
