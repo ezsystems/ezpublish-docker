f5a6c0dc40013036d9d3b4a6f4975b4e6d6698e2
Vidar Langseid <vl@ez.no>
2015-03-10 13:49:41 +0100

    Fixed so that prepare_distribution_volume.sh in cli container can update db settings in parameters.yml

---
+++ dockerfiles/ubuntu/php-cli/prepare_distribution_volume.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+cd /var/www
+
+function set_permissions
+{
+    if [ "aa$APACHE_RUN_USER" == "aa" ]; then
+        APACHE_RUN_USER=www-data
+    fi
+
+    sudo setfacl -R -m u:$APACHE_RUN_USER:rwx -m u:`whoami`:rwx ezpublish/{cache,logs,config,sessions} web
+    sudo setfacl -dR -m u:$APACHE_RUN_USER:rwx -m u:`whoami`:rwx ezpublish/{cache,logs,config,sessions} web
+
+    if [ -d ezpublish_legacy ]; then
+        sudo setfacl -R -m u:$APACHE_RUN_USER:rwx -m u:`whoami`:rwx ezpublish_legacy/{design,extension,settings,var}
+        sudo setfacl -dR -m u:$APACHE_RUN_USER:rwx -m u:`whoami`:rwx ezpublish_legacy/{design,extension,settings,var}
+    fi
+}
+
+function update_parameters.yml
+{
+    if [ "aa${database_host}" != "aa" ]; then
+        sed -i "s@database_host:.*@database_host: $database_host@" ezpublish/config/parameters.yml
+    fi
+
+    if [ "aa${database_name}" != "aa" ]; then
+        sed -i "s@database_name:.*@database_name: $database_name@" ezpublish/config/parameters.yml
+    fi
+
+    if [ "aa${database_user}" != "aa" ]; then
+        sed -i "s@database_user:.*@database_user: $database_user@" ezpublish/config/parameters.yml
+    fi
+
+    if [ "aa${database_password}" != "aa" ]; then
+        sed -i "s@database_password:.*@database_password: $database_password@" ezpublish/config/parameters.yml
+    fi
+}
+
+set_permissions
+update_parameters.yml
+
+cd - > /dev/null
+
