97dba0a9cb89209e531c1a9fa891277ef66aa405
André R <ar@ez.no>
2015-05-12 23:29:03 +0200

    Change nginx contianer to extend official image

--- dockerfiles/nginx/run.sh
+++ dockerfiles/nginx/run.sh
@@ -4,27 +4,25 @@ set -e
 
 
 # Copy nginx config from [ezp_base_dir]/doc/nginx
-cp /var/www/doc/nginx/etc/nginx/sites-available/mysite.com /etc/nginx/sites-available/ezpublish
-cp -a /var/www/doc/nginx/etc/nginx/ez_params.d /etc/nginx/
-
-ln -s -f /etc/nginx/sites-available/ezpublish /etc/nginx/sites-enabled/ezpublish
+cp ${BASEDIR}/doc/nginx/etc/nginx/sites-available/mysite.com /etc/nginx/conf.d/ez.conf
+cp -a ${BASEDIR}/doc/nginx/etc/nginx/ez_params.d /etc/nginx/
 
 # Make sure nginx forwards to php5-fpm on tcp port, not unix socket
-sed -i "s@  fastcgi_pass unix:/var/run/php5-fpm.sock;@  # fastcgi_pass unix:/var/run/php5-fpm.sock;@" /etc/nginx/sites-available/ezpublish
-sed -i "s@  #fastcgi_pass 127.0.0.1:9000;@  fastcgi_pass php_fpm:${PHP_FPM_PORT_9000_TCP_PORT};@" /etc/nginx/sites-available/ezpublish
+sed -i "s@  fastcgi_pass unix:/var/run/php5-fpm.sock;@  # fastcgi_pass unix:/var/run/php5-fpm.sock;@" /etc/nginx/conf.d/ez.conf
+sed -i "s@  #fastcgi_pass 127.0.0.1:9000;@  fastcgi_pass php_fpm:${PHP_FPM_PORT_9000_TCP_PORT};@" /etc/nginx/conf.d/ez.conf
 
 # Setting environment for ezpublish ( dev/prod/behat etc )
-sed -i "s@  #fastcgi_param ENVIRONMENT dev;@  fastcgi_param ENVIRONMENT ${EZ_ENVIRONMENT};@" /etc/nginx/sites-available/ezpublish
+sed -i "s@  #fastcgi_param ENVIRONMENT dev;@  fastcgi_param ENVIRONMENT ${EZ_ENVIRONMENT};@" /etc/nginx/conf.d/ez.conf
 
-# Update port number and basedir in site-available/ezpublish
-sed -i "s@%PORT%@${PORT}@" /etc/nginx/sites-available/ezpublish
-sed -i "s@%BASEDIR%@${BASEDIR}@" /etc/nginx/sites-available/ezpublish
+# Update port number and basedir in /etc/nginx/conf.d/ez.conf
+sed -i "s@%PORT%@${PORT}@" /etc/nginx/conf.d/ez.conf
+sed -i "s@%BASEDIR%@${BASEDIR}@" /etc/nginx/conf.d/ez.conf
 
 echo "fastcgi_read_timeout $FASTCGI_READ_TIMEOUT;" > /etc/nginx/conf.d/fastcgi_read_timeout.conf
 
 if [ aa$VARNISH_ENABLED == "aayes" ]; then
-    sed -i "s@  #fastcgi_param USE_HTTP_CACHE 1;@  fastcgi_param USE_HTTP_CACHE 0;@" /etc/nginx/sites-available/ezpublish
-    sed -i "s@  #fastcgi_param TRUSTED_PROXIES \"%PROXY%\";@  fastcgi_param TRUSTED_PROXIES \"${DOCKER0NET}\";@" /etc/nginx/sites-available/ezpublish
+    sed -i "s@  #fastcgi_param USE_HTTP_CACHE 1;@  fastcgi_param USE_HTTP_CACHE 0;@" /etc/nginx/conf.d/ez.conf
+    sed -i "s@  #fastcgi_param TRUSTED_PROXIES \"%PROXY%\";@  fastcgi_param TRUSTED_PROXIES \"${DOCKER0NET}\";@" /etc/nginx/conf.d/ez.conf
 fi
 
 
