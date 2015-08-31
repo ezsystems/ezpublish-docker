9fe1d406c0dbaeff50e6aa902e06b6f530ddf142
Vidar Langseid <vl@ez.no>
2015-08-31 15:12:38 +0200

    Added jarnaiz/behat-junit-formatter

--- resources/behat/patch_behat.yml.sh
+++ resources/behat/patch_behat.yml.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+patch -p0 < /behat.yml.dist.add_junit_formatter.patch
 cp behat.yml.dist behat.yml
 patch -p0 < /behat.yml.patch
 
+composer require --dev jarnaiz/behat-junit-formatter
