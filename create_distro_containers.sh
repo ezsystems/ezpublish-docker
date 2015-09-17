70603ac88dee945b23343775df9ec0127f88b0b5
Vidar Langseid <vl@ez.no>
2015-09-17 12:55:35 +0200

    Revert "Fixed so that 'create_distro_containers.sh --cleanup' re-creates volumes/ezpublish/.keep"
    This reverts commit 5a94d837b24528fe4b80138871ec98fdc3e3e4f7.

--- create_distro_containers.sh
+++ create_distro_containers.sh
@@ -117,7 +117,6 @@ function prepare
     if [ $REBUILD_EZP == "true" ]; then
         sudo rm -rf volumes/ezpublish volumes/mysql || /bin/true
         mkdir volumes/ezpublish volumes/mysql
-        touch volumes/ezpublish/.keep
         touch volumes/mysql/.keep
     fi
     rm -f dockerfiles/distribution/ezpublish.tar.gz
