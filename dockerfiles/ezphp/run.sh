ezsystems/ezpublish-docker

commit 54efa3604f1ee1906245f00763cf1b079e5f2e6c

Added support for SKIP_INITIALIZING_VAR variable in phpfpm image

dockerfiles/ezphp/run.sh
@@ -21,6 +21,14 @@ function parseCommandlineOptions
     if [ "$APACHE_RUN_USER" == "" ]; then
         APACHE_RUN_USER=www-data
     fi
+
+    # You might set SKIP_INITIALIZING_VAR=true if you would like to setup web/var from outside this container
+    if [ "$SKIP_INITIALIZING_VAR" == "true" ]; then
+        VARDIR=""
+    else
+        SKIP_INITIALIZING_VAR == "false"
+        VARDIR=" web/var"
+    fi
 }
 
 function getAppFolder
@@ -48,16 +56,16 @@ fi
 
 
 echo "Setting permissions on eZ Publish folder as they might be broken if rsync is used"
-if [ ! -d web/var ]; then
+if [ ! -d web/var ] && [ "$SKIP_INITIALIZING_VAR" == "false" ]; then
     sudo -u ez mkdir web/var
 fi
 
 if [ -d ezpublish ]; then
-    setfacl -R -m u:$APACHE_RUN_USER:rwX -m u:ez:rwX ezpublish/{cache,logs,sessions} web/var
-    setfacl -dR -m u:$APACHE_RUN_USER:rwX -m u:ez:rwX ezpublish/{cache,logs,sessions} web/var
+    setfacl -R -m u:$APACHE_RUN_USER:rwX -m u:ez:rwX ezpublish/{cache,logs,sessions}${VARDIR}
+    setfacl -dR -m u:$APACHE_RUN_USER:rwX -m u:ez:rwX ezpublish/{cache,logs,sessions}${VARDIR}
 else
-    setfacl -R -m u:$APACHE_RUN_USER:rwX -m u:ez:rwX app/{cache,logs} web/var
-    setfacl -dR -m u:$APACHE_RUN_USER:rwX -m u:ez:rwX app/{cache,logs} web/var
+    setfacl -R -m u:$APACHE_RUN_USER:rwX -m u:ez:rwX app/{cache,logs}${VARDIR}
+    setfacl -dR -m u:$APACHE_RUN_USER:rwX -m u:ez:rwX app/{cache,logs}${VARDIR}
 fi
 
 if [ -d ezpublish_legacy ]; then

commit 8f1a32dc3db03993f4fa5fddf501eeb1b071dc13

Fixed typo introduced in 54efa3604f1ee1906245f00763cf1b079e5f2e6c causing run.sh to misbehave if SKIP_INITIALIZING_VAR was not set

dockerfiles/ezphp/run.sh
@@ -26,7 +26,7 @@ function parseCommandlineOptions
     if [ "$SKIP_INITIALIZING_VAR" == "true" ]; then
         VARDIR=""
     else
-        SKIP_INITIALIZING_VAR == "false"
+        SKIP_INITIALIZING_VAR="false"
         VARDIR=" web/var"
     fi
 }